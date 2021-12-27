156a8608b5d930336602b64731b8ace8e544191e
Ali Hashemi <alihashemmi.ca@gmail.com>
2021-12-27 12:33:59 -0500

    updated

--- javascript-practice/directions.js
+++ javascript-practice/directions.js
@@ -5,23 +5,20 @@ const finalPosition = function (moves) {
   let y = 0;
   for (let i = 0; i < moves.length ; i++) {
 
-   if (moves[i] === 'north') {
-     y++;
-   }
-   if (moves[i] === 'south') {
-     y--;
-   }
+    if (moves[i] === 'north') {
+      y++;
+    }
+    if (moves[i] === 'south') {
+      y--;
+    }
     if (moves[i] === 'west') {
-    x--;
-   }
-   if (moves[i] === 'east') {
-    x++;
-   }
+      x--;
+    }
+    if (moves[i] === 'east') {
+      x++;
+    }
   }
-return [x,y];
+  return [x,y];
 
 };
 console.log(finalPosition(moves));
-
-
-
